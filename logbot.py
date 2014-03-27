756f3f6472055e318b3acca9887f13a399c91e40
Niels Martin Hansen <nielsm@indvikleren.dk>
2014-03-27 22:39:00 +0100

    More Unicode

--- logbot.py
+++ logbot.py
@@ -12,13 +12,13 @@ class LogBot(irc.bot.SingleServerIRCBot):
     self.config = config
     self.desired_channels = irc.dict.IRCDict((ch, True) for ch in config.channels)
 
-    print "Connect to database"
+    print u"Connect to database"
     self.db = redis.StrictRedis(**config.redis)
 
     db_channels = self.db.lrange('cfg:channels', 0, -1)
-    print "Loaded {} channels from config database".format(len(db_channels))
-    self.desired_channels.update((ch, True) for ch in db_channels)
-    print "Total {} channels to join".format(len(self.desired_channels))
+    print u"Loaded {} channels from config database".format(len(db_channels))
+    self.desired_channels.update((ch.decode('utf-8'), True) for ch in db_channels)
+    print u"Total {} channels to join".format(len(self.desired_channels))
 
     super(LogBot, self).__init__(config.servers, config.nick, config.realname)
 
@@ -28,7 +28,7 @@ class LogBot(irc.bot.SingleServerIRCBot):
 
   def save_config(self):
     self.db.delete('cfg:channels')
-    self.db.lpush('cfg:channels', *self.desired_channels.keys())
+    self.db.lpush('cfg:channels', *[ch.encode('utf-8') for ch in self.desired_channels.keys()])
 
   def add_log(self, logdata, channels):
     # log the given data for the given list of channels
@@ -43,27 +43,27 @@ class LogBot(irc.bot.SingleServerIRCBot):
     self.db.hmset(evname, logdata)
     self.db.expire(evname, self.config.expiretime)
     for ch in channels:
-      self.db.lpush('log:{}'.format(ch), evname)
+      self.db.lpush('log:{}'.format(ch.encode('utf-8')), evname)
 
   def get_userchannels(self, usernick):
     return [chn for (chn, cho) in self.channels.iteritems() if cho.has_user(usernick)]
 
   def on_nicknameinuse(self, conn, ev):
     new_nick = self.config.nick + random.randint(10, 99)
-    print "Nick in use, instead trying {}".format(new_nick)
+    print u"Nick in use, instead trying {}".format(new_nick)
     conn.nick(new_nick)
     def reset_nick():
-      print "Trying to switch to configured nick"
+      print u"Trying to switch to configured nick"
       conn.nick(self.config.nick)
     conn.execute_delayed(30, reset_nick)
 
   def get_version(self):
-    return "Channel logger bot ({})".format(super(LogBot, self).get_version())
+    return u"Channel logger bot ({})".format(super(LogBot, self).get_version())
 
   def on_welcome(self, conn, ev):
-    print "Connected!"
+    print u"Connected!"
     for chan in self.desired_channels.keys():
-      print "Join channel {}".format(chan)
+      print u"Join channel {}".format(chan)
       conn.join(chan)
     self.save_config()
 
@@ -74,7 +74,7 @@ class LogBot(irc.bot.SingleServerIRCBot):
     channel = ev.arguments[0]
     self.desired_channels[channel] = True
     conn.join(channel)
-    print "Invited to {} by {}".format(channel, ev.source)
+    print u"Invited to {} by {}".format(channel, ev.source)
     self.save_config()
 
   def on_kick(self, conn, ev):
@@ -83,7 +83,7 @@ class LogBot(irc.bot.SingleServerIRCBot):
     message = ev.arguments[1]
     if kicked == conn.get_nickname():
       del self.desired_channels[channel]
-      print "Kicked from {} by {}".format(channel, ev.source)
+      print u"Kicked from {} by {}".format(channel, ev.source)
       self.save_config()
       self.add_log({'event': 'endlog'}, [ev.target])
     else:
@@ -123,7 +123,7 @@ class LogBot(irc.bot.SingleServerIRCBot):
 
   def on_privmsg(self, conn, ev):
     message = ev.arguments[0]
-    conn.privmsg(ev.source.nick, "Hi {}, you said: {}".format(ev.source, message))
+    conn.privmsg(ev.source.nick, u"Hi {}, you said: {}".format(ev.source, message))
 
   def on_pubmsg(self, conn, ev):
     logdata = {
@@ -164,12 +164,15 @@ class LogBot(irc.bot.SingleServerIRCBot):
     }
     self.add_log(logdata, [ev.target])
 
+  def disconnect(self, message=''):
+    self.add_log({'event': 'endlog'}, [ch.encode('utf-8') for ch in self.channels.keys()])
+    self.connection.disconnect(message)
 
 def main():
   try:
     import config
   except Exception as e:
-    print "Could not import config"
+    print u"Could not import config"
     print e
     exit(1)
 
@@ -180,7 +183,7 @@ def main():
     print "Now dying..."
     bot.save_config()
     bot.db.save()
-    bot.connection.disconnect("Death by console")
+    bot.disconnect(u"Death by console")
     print "Dead."
 
 if __name__ == "__main__":
