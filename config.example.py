830e046f5feaff4241c44f4117275f3bbd41cd31
Niels Martin Hansen <nielsm@indvikleren.dk>
2014-03-31 13:16:51 +0200

    More reasonable log url format example

--- config.example.py
+++ config.example.py
@@ -10,5 +10,5 @@ redis = dict(host='localhost', port=6379, db=0)
 management_password = "password"
 expiretime = 6*60*60  # 6 hours
 maxlogentries = 200
-log_read_url_format = u"http://logs.example.com/readlog?ch={channel}&tk={token}"
+log_read_url_format = u"{channel: <15s}: http://logs.example.com/{token}"
 token_duration = 300  # 5 minutes
